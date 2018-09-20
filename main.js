fb934f7489b11d071cff6d772c4a6676a16e62f9
Allysa <allysaalzona@gmail.com>
2018-09-20 17:22:52 +0800

    Modified changeSlideshowImages()

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 // Change slideshow images for mobile view
 function changeSlideshowImages(x) {
+
+    var activeSlide = document.getElementsByClassName('carousel-item active');
+    var slides = document.getElementsByClassName('carousel-item');
+
     if (x.matches) {
-        var activeSlide = document.getElementsByClassName('carousel-item active');
         activeSlide.item(0).innerHTML = '<img class="d-block w-100" src="assets/homeslidemob.png" alt="First slide">'
 
-       
-        var slides = document.getElementsByClassName('carousel-item');
         for (var i=1; i<slides.length; i++) {
             slides.item(i).innerHTML = '<img class="d-block w-100" src="assets/Slide'+(i+1)+
             'mobileview1.png">';
         }
     }
+    else {
+        activeSlide.item(0).innerHTML = '<img class="d-block w-100" src="assets/newHomeslide.png" alt="First slide">'
+
+        for (var i=1; i<slides.length; i++) {
+            slides.item(i).innerHTML = '<img class="d-block w-100" src="assets/Slide'+(i+1)+
+            '_1.png">';
+        }
+    }
 }
 
+// Event listener for screen size change
 var x = window.matchMedia("(max-width: 576px)");
 changeSlideshowImages(x);
+x.addListener(changeSlideshowImages);
